04cf663939d96000dd1691bd64f0c92c7815d68a
Poschi17 <posch220085@sr.htlweiz.at>
2025-11-10 08:16:51 +0100

    A1 Stop Threads

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -9,32 +9,48 @@ class Program
 
     public static void Main(string[] args)
     {
+        int countA = 1;
+        int countB = 100;
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
-        Thread threadA = new Thread(CountUpThreadA);
-        Thread threadB = new Thread(CountDownThreadB);
+        Thread threadA = new Thread(() => CountUpThreadA(countA));
+        Thread threadB = new Thread(() => CountDownThreadB(countB));
+        Thread threadCheck = new Thread(() => CheckCount(countA, countB, threadA, threadB));
         threadA.Start();
         threadB.Start();
-        threadA.Join();
-        threadB.Join();
+        threadCheck.Start();
+        Console.WriteLine(countA);
     }
 
-    private static void CountUpThreadA()
+    private static void CountUpThreadA(int countA)
     {
-        int count = 1;
-        while (count < 100)
+        while (countA < 100)
         {
-            count++;
+            countA++;
             Thread.Sleep(100);
         }
     }
 
-    private static void CountDownThreadB()
+    private static void CountDownThreadB(int countB)
     {
-        int count = 100;
-        while (count > 1)
+        while (countB > 1)
         {
-            count--;
+            countB--;
             Thread.Sleep(100);
         }
     }
+
+    private static int CheckCount(int countA, int countB, Thread threadA, Thread threadB)
+    {
+        while (countA != countB)
+        {
+            if (countA == countB)
+            {
+                threadA.Join();
+                threadB.Join();
+                return countA;
+            }
+        }
+        return 0;
+    }
+
 }

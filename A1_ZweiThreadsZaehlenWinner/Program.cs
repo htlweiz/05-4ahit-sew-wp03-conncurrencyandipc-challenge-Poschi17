7cd1f5b702f156ff869e3d04dd237e1b31d3d281
Poschi17 <posch220085@sr.htlweiz.at>
2025-11-10 08:01:03 +0100

    A1 Finish

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -5,22 +5,36 @@ namespace A1_ZweiThreadsZaehlenWinner;
 
 class Program
 {
-   
-    
+
+
     public static void Main(string[] args)
     {
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
-        
-        
+        Thread threadA = new Thread(CountUpThreadA);
+        Thread threadB = new Thread(CountDownThreadB);
+        threadA.Start();
+        threadB.Start();
+        threadA.Join();
+        threadB.Join();
     }
-    
+
     private static void CountUpThreadA()
     {
-        
+        int count = 1;
+        while (count < 100)
+        {
+            count++;
+            Thread.Sleep(100);
+        }
     }
-    
+
     private static void CountDownThreadB()
     {
-       
+        int count = 100;
+        while (count > 1)
+        {
+            count--;
+            Thread.Sleep(100);
+        }
     }
 }
